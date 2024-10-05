23f738658389e4840be74cf59e0dda9652db4ca1
Riniinfanceya <156685015+Riniinfanceya@users.noreply.github.com>
2024-10-05 06:09:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './comp/Greet'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet/>
+    </div>
+  );
+
+}
+
+export default App;
